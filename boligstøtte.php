d0c7e0351df4355e1204aec9f10117d59baaa671
421387 <email>
2016-01-14 11:31:35 +0100

    code
    examples

---
+++ boligstøtte.php
@@ -0,0 +1,93 @@
+<script type="application/ld+json">
+{
+	"@context": "http://schema.org",
+	"@type": "GovernmentService",
+	"name": "Boligstøtte",
+	"description": "Boligstøtte er et tilskud til huslejen. Du kan søge boligstøtte, hvis du bor til leje. 
+					Er du folke- eller førtidspensionist, kan du også søge boligstøtte til en ejer- eller andelsbolig.",
+	"url": "https://www.kolding.dk/borger/bolig-byggeri-flytning/boligstotte",
+	"serviceType": "Boligstøtte",
+	"offers": {
+		"@type": "Offer",
+		"name": "Boligstøtte"
+	},
+	"serviceOperator": {
+		"@type": "GovernmentOrganization",
+		"name": "Udbetaling Danmark",
+		"url": "https://www.borger.dk/om-borger-dk/Sider/udbetaling-danmark.aspx",
+		"sameAs": "https://www.atp.dk/atp-som-administrator/udbetaling-danmark"
+	},
+	
+  	"areaServed": {
+    	"@type": "Country",
+    	"name": "Danmark"
+
+  	},
+	  "hasOfferCatalog": {
+	    "@type": "OfferCatalog",
+	    "name": "Boligstøtte",
+	    "url": "https://www.borger.dk/Sider/Boligstoette-soege.aspx",
+	    "itemListElement": [
+	      {
+	        "@type": "OfferCatalog",
+	        "name": "Boligstøtte",
+	        "description": "Hvem kan få boligstøtte? - Boligstøtte er et tilskud til huslejen. Du kan søge boligstøtte, hvis du bor til leje. Er du folke- eller førtidspensionist, kan du også søge boligstøtte til en ejer- eller andelsbolig.",
+	        "itemListElement": [
+	          {
+	            "@type": "Offer",
+	            "itemOffered": {
+	              "@type": "Service",
+	              "description": "Søg boligstøtte for almindelige borgere",
+	              "name": "Boligstøtte til lejere"
+	            },
+	           	"@type": "Offer",
+	            "itemOffered": {
+	              "@type": "Service",
+	              "description": "Søg boligstøtte for Folkepensionister",
+	              "name": "Boligstøtte til Folkepensionister"
+	          	},
+	          	"@type": "Offer",
+	            "itemOffered": {
+	              "@type": "Service",
+	              "description": "Søg boligstøtte for førtidspensionister",
+	              "name": "Boligstøtte til førtidspensionister"
+	            }
+	          }
+	        ]
+	      },
+	      {
+	        "@type": "OfferCatalog",
+	        "name": "Beregn Boligstøtte",
+	        "description": "Hvor meget kan jeg få i boligstøtte? - Du skal bo i en bolig med eget køkken for at få boligstøtte.",
+	        "url": "https://www.borger.dk/Sider/handlingsside.aspx?DomainServiceId=72b5638f-70c3-41e3-8974-e7888099d981&PageId=b0244c66-1e26-470c-88c1-768a24b0abc8&KommuneKode=621",
+	        "itemListElement": [
+	          {
+	            "@type": "Offer",
+	            "itemOffered": {
+	              "@type": "Service",
+	              "description": "Beregn din boligstøtte",
+	              "name": "Boligstøtte Beregner"
+	            }
+	          }
+	        ]
+	      },
+	      {
+	        "@type": "OfferCatalog",
+	        "name": "Lån til Beboerindskud",
+	         "description": "Lån til beboerindskud - Hvis du flytter i en almen bolig, kan du søge om Beboerindskudslån",
+	        "url": "https://www.borger.dk/Sider/Laan-til-beboerindskud.aspx?KommuneKode=621",
+	        "itemListElement": [
+	          {
+	            "@type": "Offer",
+	            "itemOffered": {
+	              "@type": "Service",
+	               "description": "Beboerindskudslån ",
+	              "name": "Beboerindskudslån"
+	            }
+	          }
+	        ]
+	      }
+	    ]
+	}
+}
+</script>
---
+++ sygedagpenge.php
@@ -0,0 +1,51 @@
+<script type='application/ld+json'>
+{
+  "@context": "http://schema.org",
+  "@type": "GovernmentService",
+  "name": "Sygedagpenge",
+  "description": "Kolding Kommune's sygedagpenge område. 
+                  Du kan få sygedagpenge, hvis du er ude af stand til at arbejde pga. sygdom eller fordi, du er kommet til skade.",
+  "serviceType": "Sygedagpenge",
+
+  "provider": {
+    "@type": "GovernmentOrganization",
+    "name": "Kolding Kommune",
+    "url": "https://www.kolding.dk/borger/arbejde-ledighed/sygedagpenge",
+    "sameAs": "https://da.wikipedia.org/wiki/Kolding_Kommune"
+  },
+  
+  "serviceArea": {
+    "@type": "AdministrativeArea",
+    "name": "Kolding Kommune / Borger / Arbejde & Ledighed / Sygedagpenge"
+  },
+  
+  "areaServed": {
+  "@type": "City",
+  "name": "Kolding Kommune"
+  },
+
+  "offers": {
+    "@type": "Offer",
+    "name": "Søg Sygedagpenge",
+    "url": "http://www.xn--mitsygefravr-hdb.dk/"
+  }
+}
+</script>
+
+<script type='application/ld+json'>
+{
+  "@context": "http://schema.org",
+    "@type": "CreativeWork",
+    "video": {
+    "@type": "VideoObject",
+    "description": "Introduktion til Mit Sygefravær from KOMBIT on Vimeo",
+    "duration": "T2M24S",
+    "name": "Søg sygedagpenge",
+    "uploadDate": "2015-01-06",
+    "thumbnailUrl": "http://i.imgur.com/346pgkF.png",
+    "citation": "http://www.kombit.dk/",
+    "educationalUse": "Hvorår kan du få sygedagpenge?",
+    "sameAs": "https://vimeo.com/128977191"
+  }
+}
+</script>
